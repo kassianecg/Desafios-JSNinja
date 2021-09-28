efc1579158c7619e2f2f3751df2faead99668739
Kassiane CG <kassianecristinagoncalves@gmail.com>
2021-09-28 10:58:25 -0300

    desafio 1 completo

---
+++ desafio-01/index.js
@@ -0,0 +1,20 @@
+var myvar;
+myvar = 10;
+var soma = 15 + 8;
+soma++;
+soma = soma * 3;
+console.log(soma);
+
+var souninja = true;
+var comida = ['arroz', 'feijão', 'ovo'];
+console.log(comida[1]);
+
+console.log('Verifica se soma é igual a myvar:', soma === myvar);
+
+console.log('Verifica se a variável soma é igual a variável myvar:', myvar <= myvar);
+
+function divisao(x,y){
+    console.log(x/y); 
+}
+divisao(10,2);
+
