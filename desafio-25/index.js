aff0dafbcb959d4621284e397cc25d2527d3c80d
Kassiane CG <kassianecristinagoncalves@gmail.com>
2021-11-02 23:37:44 -0300

    desafio-25 completed

---
+++ desafio-25/index.js
@@ -0,0 +1,13 @@
+/*
+  Retirar duvidas com o meu mentor
+
+    -Map
+    -Some
+    -Every
+    
+  Alguns items sobre HTML
+
+    -Events
+    -data-js (Pelo que entendi é uma props customizada definida por mim)
+
+*/
